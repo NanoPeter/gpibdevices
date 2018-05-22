2835566f25e074669dc19dea2fdad999453c50e9
Peter Gruszka <gruszka@physik.uni-frankfurt.de>
2018-05-22 13:27:03 +0200

    now you can use both channels of the sourcemeter2602A

--- keithley/sourcemeter2602A.py
+++ keithley/sourcemeter2602A.py
@@ -1,47 +1,55 @@
+from enum import Enum
+
+
+class SMUChannel(Enum):
+    channelA = 'a'
+    channelB = 'b'
 
 class Sourcemeter2602A(object):
-        def __init__(self, device):
-                self.__dev = device
-                self.__dev.write('smua.reset()')
-               
+        def __init__(self, device, sub_device: SMUChannel = SMUChannel.channelA):
+                self._dev = device
+                self.__channel_string = 'smu{}'.format(sub_device.value)
+                self.__channel_token = sub_device.value
+                self._dev.write('smua.reset()')
+
         def voltage_driven(self, voltage, current_limit=1e-6, nplc = 3):
 
-                self.__dev.write('smua.reset()')
-                self.__dev.write("smua.source.func = smua.OUTPUT_DCVOLTS")
-                self.__dev.write("smua.source.levelv = 0.0")
-                self.__dev.write("smua.source.autorangev = smua.AUTORANGE_ON")
-                self.__dev.write("smua.source.limiti = {}".format(current_limit))
-                self.__dev.write("smua.measure.autorangei = smua.AUTORANGE_ON")
-                self.__dev.write("smua.measure.nplc = {}".format(nplc))
-                
+                self._dev.write('{}.reset()'.format(self.__channel_string))
+                self._dev.write("{}.source.func = smua.OUTPUT_DCVOLTS".format(self.__channel_string))
+                self._dev.write("{}.source.levelv = 0.0".format(self.__channel_string))
+                self._dev.write("{}.source.autorangev = smua.AUTORANGE_ON".format(self.__channel_string))
+                self._dev.write("{}.source.limiti = {}".format(self.__channel_string, current_limit))
+                self._dev.write("{}.measure.autorangei = smua.AUTORANGE_ON".format(self.__channel_string))
+                self._dev.write("{}.measure.nplc = {}".format(self.__channel_string, nplc))
+
         def current_driven(self, current, voltage_limit=1):
                 raise NotImplementedError('you fool!')
-            
+
         def arm(self):
-                self.__dev.write("smua.source.output = smua.OUTPUT_ON")
+                self._dev.write("{}.source.output = smua.OUTPUT_ON".format(self.__channel_string))
 
         def disarm(self):
-                self.__dev.write("smua.source.output = smua.OUTPUT_OFF")
-           
+                self._dev.write("{}.source.output = smua.OUTPUT_OFF".format(self.__channel_string))
+
         def set_voltage(self, voltage):
-                self.__dev.write("smua.source.levelv = {}".format(voltage))
+                self._dev.write("{}.source.levelv = {}".format(self.__channel_string, voltage))
 
         def set_current(self, current):
                 raise NotImplementedError('you fool!')
 
         def read(self):
-                self.__dev.write("ireading, vreading = smua.measure.iv()")
-                return self.__dev.query_ascii_values("printnumber(vreading,ireading)")
+                self._dev.write("ireading{0}, vreading{0} = {1}.measure.iv()".format(self.__channel_token, self.__channel_string))
+                return self._dev.query_ascii_values("printnumber(vreading{0},ireading{0})".format(self.__channel_token))
 
 
 
 if __name__ == '__main__':
         from visa import ResourceManager as RM
-        
+
         rm = RM('@py')
-        
+
         dev = rm.open_resource('GPIB0::10::INSTR')
-        
+
         smu = Sourcemeter2602A(dev)
         smu.voltage_driven(0)
         smu.arm()
@@ -49,4 +57,4 @@ if __name__ == '__main__':
         print(smu.read())
         smu.set_voltage(0)
         smu.disarm()
-        
+
