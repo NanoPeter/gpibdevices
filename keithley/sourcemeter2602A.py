3580a7750f178c37126631ae5139e1f9c2d5d56b
localuser <localuser@aghuth.de>
2018-06-13 12:20:49 +0200

    Fixed error in SMU 2363A class where current limit was not set.

--- keithley/sourcemeter2602A.py
+++ keithley/sourcemeter2602A.py
@@ -12,15 +12,15 @@ class Sourcemeter2602A(object):
         self._channel_string = 'smu{}'.format(sub_device.value)
         self._channel_token = sub_device.value
         self._channel = sub_device
-        self._dev.write('smua.reset()')
+        self._dev.write('{0}.reset()'.format(self._channel_string))
 
     def voltage_driven(self, voltage, current_limit=1e-6, nplc = 3, range=1e-8):
         self._dev.write('{}.reset()'.format(self._channel_string))
-        self._dev.write("{}.source.func = smua.OUTPUT_DCVOLTS".format(self._channel_string))
+        self._dev.write("{0}.source.func = {0}.OUTPUT_DCVOLTS".format(self._channel_string))
         self._dev.write("{}.source.levelv = 0.0".format(self._channel_string))
-        self._dev.write("{}.source.autorangev = smua.AUTORANGE_ON".format(self._channel_string))
+        self._dev.write("{0}.source.autorangev = {0}.AUTORANGE_ON".format(self._channel_string))
         self._dev.write("{}.source.limiti = {}".format(self._channel_string, current_limit))
-        self._dev.write("{}.measure.autorangei = smua.AUTORANGE_ON".format(self._channel_string))
+        self._dev.write("{0}.measure.autorangei = {0}.AUTORANGE_ON".format(self._channel_string))
         self._dev.write("{}.measure.nplc = {}".format(self._channel_string, nplc))
         self._dev.write("{}.measure.lowrangei = {}".format(self._channel_string, range))
 
@@ -28,10 +28,10 @@ class Sourcemeter2602A(object):
         raise NotImplementedError('you fool!')
 
     def arm(self):
-        self._dev.write("{}.source.output = smua.OUTPUT_ON".format(self._channel_string))
+        self._dev.write("{0}.source.output = {0}.OUTPUT_ON".format(self._channel_string))
 
     def disarm(self):
-        self._dev.write("{}.source.output = smua.OUTPUT_OFF".format(self._channel_string))
+        self._dev.write("{0}.source.output = {0}.OUTPUT_OFF".format(self._channel_string))
 
     def set_voltage(self, voltage):
         self._dev.write("{}.source.levelv = {}".format(self._channel_string, voltage))
@@ -55,10 +55,6 @@ if __name__ == '__main__':
     dev = rm.open_resource('GPIB0::10::INSTR')
 
     smu = Sourcemeter2602A(dev)
-    smu.voltage_driven(0)
-    smu.arm()
-    smu.set_voltage(1e-3)
-    print(smu.read())
-    smu.set_voltage(0)
-    smu.disarm()
+    smu.voltage_driven(0, 1e-9, 1, range=1e-12)
+
 
